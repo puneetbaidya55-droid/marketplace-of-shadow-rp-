c1e883275dee0bbca1ce9641192124e84a8ed843
puneetbaidya55-droid <puneetbaidya55@gmail.com>
2026-06-05 14:31:14 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+function add(name, price){
+    cart.push({name, price});
+    localStorage.setItem("cart", JSON.stringify(cart));
+    alert(name + " added to cart!");
+}
